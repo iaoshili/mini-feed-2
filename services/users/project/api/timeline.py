b25c46e2ba0bb6e47ac1d203484339be07fad96b
Li Aoshi <liaoshi@Lis-MacBook.fibrestream.ca>
2022-07-03 18:09:41 -0400

    Added global cache

--- services/users/project/api/timeline.py
+++ services/users/project/api/timeline.py
@@ -4,6 +4,7 @@
 from flask_restx import Namespace, Resource
 from flask import request
 from project.api.users.crud import get_user_by_name, follow, get_timeline
+from project.cache.global_cache import cached
 
 timeline_namespace = Namespace("timeline")
 
@@ -12,11 +13,16 @@ timeline_namespace = Namespace("timeline")
 @timeline_namespace.param('username')
 class TimelineResource(Resource):
     def get(self, username):
-        user = get_user_by_name(username)
+        return _get_timeline(username)
 
-        feeds = get_timeline(user_id=user.id)
 
-        return [feed.content for feed in feeds], 201
+@cached
+def _get_timeline(username):
+    user = get_user_by_name(username)
+
+    feeds = get_timeline(user_id=user.id)
+
+    return [feed.content for feed in feeds], 201
 
 
 timeline_namespace.add_resource(TimelineResource, "")
--- services/users/project/cache/global_cache.py
+++ services/users/project/cache/global_cache.py
@@ -1,3 +1,6 @@
+import json
+from functools import wraps
+
 import redis
 
 
@@ -5,3 +8,33 @@ def redis_connect() -> redis.client.Redis:
     return redis.Redis(host='redis', port=6379, db=0, password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81")
 
 global_cache = redis_connect()
+
+
+# From https://gist.github.com/mminer/34d4746fa82b75182ee7
+def cached(func):
+    """
+    Decorator that caches the results of the function call.
+
+    We use Redis in this example, but any cache (e.g. memcached) will work.
+    We also assume that the result of the function can be seralized as JSON,
+    which obviously will be untrue in many situations. Tweak as needed.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Generate the cache key from the function's arguments.
+        key_parts = [func.__name__] + list(args)
+        key = '-'.join(key_parts)
+        result = global_cache.get(key)
+
+        if result is None:
+            # Run the function and cache the result for next time.
+            value = func(*args, **kwargs)
+            value_json = json.dumps(value)
+            global_cache.set(key, value_json)
+        else:
+            # Skip the function entirely and use the cached value instead.
+            value_json = result.decode('utf-8')
+            value = json.loads(value_json)
+
+        return value
+    return wrapper
